253821b60a405c8286336de9e791f53ee47e4918
Firas Mostafa <frasm688@gmail.com>
2026-08-14 20:37:35 +0300

    add semantic htmlk and open graphe meta tags

---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+function Header() {
+  return (
+    <header>
+      <img src="/logo.svg" alt="Little Lemon Logo" />
+    </header>
+  );
+}
+
+export default Header;
---
+++ src/components/Main.js
@@ -0,0 +1,10 @@
+function Main() {
+  return (
+    <main>
+      <h2>Welcome to Little Lemon</h2>
+      <p>Enjoy delicious food and book your table with us.</p>
+    </main>
+  );
+}
+
+export default Main;
